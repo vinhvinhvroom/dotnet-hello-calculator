bf413bbb5f13501c9c94cd67e362a0712c79e0e7
vinhvinhvroom <vinh.doan13@gmail.com>
2020-03-23 14:06:34 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace dotnet_hello_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = AddNumbers(64, 32);
+            Console.WriteLine(result); 
+            Console.ReadKey();
+        }
+
+        private static int AddNumbers(int numberOne, int numberTwo)
+        {
+            int CalculationResult; 
+            CalculationResult = numberOne + numberTwo; 
+            return CalculationResult;
+        }
+
+    }
+}
